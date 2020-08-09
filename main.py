ce7f6f86d557fdb3dce8449965ecaddab246a238
AJJAPUSIVA <siva.ajjapu96@gmail.com>
2020-08-09 15:07:54 +0200

    add minimum

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import statistics
 
 app = Flask(__name__)
 
@@ -13,6 +14,34 @@ def addition():
     value2=request.args.get('B',default = 0, type = int)
     result=value1+value2
     return '%d \n' % result
+
+
+def Inputs_method():
+    try:
+        inputs = request.args.get('X', type=str)
+        inputs = inputs.split(',')
+        values = []
+        for value in inputs:
+            value = float(value)
+            values.append(value)
+        return values
+    except ValueError:
+        Error_msg = "There are error values in inputs, Please provide a list of values,for example 1,2,3,4"
+        return Error_msg
+
+
+@app.route('/min')
+def minimum():
+    values = Inputs_method()
+    if not type(values) is str:
+        values.sort()
+        if (values[0].is_integer()):
+            return str(int(values[0]))
+        else:
+            return str(values[0])
+    else:
+        return values
+
 
 
 if __name__ == "__main__":

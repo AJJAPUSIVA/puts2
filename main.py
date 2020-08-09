a592db7852dbbc313d7efd1c951e153800799731
ajjapusiva <siva.ajjapu96@gmail.com>
2020-08-09 21:38:09 +0200

    reformat main and test

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "Usage;\n<Operation>?X=<Value1, Value2, ..., ValueN>\n"
+
 
 if __name__ == "__main__":
     app.run()

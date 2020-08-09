84eeda294082e4876c8f6aadf10495bbecafbf11
AJJAPUSIVA <siva.ajjapu96@gmail.com>
2020-08-09 14:58:58 +0200

    new update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-import statistics
 
 app = Flask(__name__)
 
@@ -10,9 +9,9 @@ def index():
 
 @app.route('/add')
 def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
+    value1 = request.args.get('A', default=0, type=int)
+    value2 = request.args.get('B', default=0, type=int)
+    result = value1 + value2
     return '%d \n' % result
 
 
@@ -28,6 +27,8 @@ def Inputs_method():
     except ValueError:
         Error_msg = "There are error values in inputs, Please provide a list of values,for example 1,2,3,4"
         return Error_msg
+
+
 
 
 if __name__ == "__main__":

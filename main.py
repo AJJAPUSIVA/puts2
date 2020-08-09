6e403c20a2947384907cfb0c64fd245561f8918f
ajjapusiva <siva.ajjapu96@gmail.com>
2020-08-09 21:10:25 +0200

    Add unit tests

--- main.py
+++ main.py
@@ -3,18 +3,6 @@ import statistics
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
-
-
-@app.route('/add')
-def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
-
 
 def Inputs_method():
     try:
@@ -26,7 +14,7 @@ def Inputs_method():
             values.append(value)
         return values
     except ValueError:
-        Error_msg = "There are error values in inputs, Please provide a list of values,for example 1,2,3,4"
+        Error_msg = "There are error values in inputs, Please provide a list of values,for example 1,2,3,4\n"
         return Error_msg
 
 
@@ -34,11 +22,11 @@ def Inputs_method():
 def average():
     values = Inputs_method()
     if not type(values) is str:
-        mean = statistics.mean(values)
-        if (mean.is_integer()):
-            return str(int(mean))
+        avrg = statistics.mean(values)
+        if avrg.is_integer():
+            return str(int(avrg)) + ' \n'
         else:
-            return str(mean)
+            return str(avrg) + ' \n'
     else:
         return values
 
@@ -47,11 +35,11 @@ def average():
 def avg():
     values = Inputs_method()
     if not type(values) is str:
-        mean = statistics.mean(values)
-        if (mean.is_integer()):
-            return str(int(mean))
+        avrg = statistics.mean(values)
+        if avrg.is_integer():
+            return str(int(avrg)) + ' \n'
         else:
-            return str(mean)
+            return str(avrg) + ' \n'
     else:
         return values
 
@@ -60,15 +48,14 @@ def avg():
 def mean():
     values = Inputs_method()
     if not type(values) is str:
-        mean = statistics.mean(values)
-        if (mean.is_integer()):
-            return str(int(mean))
+        avrg = statistics.mean(values)
+        if avrg.is_integer():
+            return str(int(avrg)) + ' \n'
         else:
-            return str(mean)
+            return str(avrg) + ' \n'
     else:
         return values
 
-        
 
 if __name__ == "__main__":
     app.run()
---
+++ test.py
@@ -0,0 +1,22 @@
+import unittest
+import main
+
+
+class TestCalculator(unittest.TestCase):
+
+    def setUp(self):
+        main.app.testing = True
+        self.app = main.app.test_client()
+
+    def testMean(self):
+        solution = self.app.get("/average?X=1,2,5,0,100")
+        self.assertEqual(b'21.6 \n', solution.data)
+    def testMean(self):
+        solution = self.app.get("/avg?X=1,2.33,4.5,3.6")
+        self.assertEqual(b'2.8575 \n', solution.data)
+    def testMean(self):
+        solution = self.app.get("/mean?X=1,2,5,0,100,-100,-5,-2")
+        self.assertEqual(b'0.125 \n', solution.data)
+
+if __name__ == '__main__':
+    unittest.main()

ecad7c43ec43f5be736038e69cba593c43220cb8
AJJAPUSIVA <siva.ajjapu96@gmail.com>
2020-08-09 15:11:08 +0200

    add median

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import statistics
 
 app = Flask(__name__)
 
@@ -14,6 +15,33 @@ def addition():
     result=value1+value2
     return '%d \n' % result
 
+
+def Inputs_method():
+    try:
+        inputs = request.args.get('X', type=str)
+        inputs = inputs.split(',')
+        values = []
+        for value in inputs:
+            value = float(value)
+            values.append(value)
+        return values
+    except ValueError:
+        Error_msg = "There are error values in inputs, Please provide a list of values,for example 1,2,3,4"
+        return Error_msg
+
+
+@app.route('/median')
+def median():
+    values = Inputs_method()
+    if not type(values) is str:
+        values.sort()
+        tempValue = statistics.median(values)
+        if (tempValue.is_integer()):
+            return str(int(tempValue))
+        else:
+            return str(tempValue)
+    else:
+        return values
 
 if __name__ == "__main__":
     app.run()

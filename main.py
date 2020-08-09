3d150ab59c34dcaca6b1062ffa72970a95b8a0a6
AJJAPUSIVA <siva.ajjapu96@gmail.com>
2020-08-09 15:02:45 +0200

    add average

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import statistics
 
 app = Flask(__name__)
 
@@ -14,6 +15,60 @@ def addition():
     result=value1+value2
     return '%d \n' % result
 
+
+def Inputs_method():
+    try:
+        inputs = request.args.get('X', type=str)
+        inputs = inputs.split(',')
+        values = []
+        for value in inputs:
+            value = float(value)
+            values.append(value)
+        return values
+    except ValueError:
+        Error_msg = "There are error values in inputs, Please provide a list of values,for example 1,2,3,4"
+        return Error_msg
+
+
+@app.route('/average')
+def average():
+    values = Inputs_method()
+    if not type(values) is str:
+        mean = statistics.mean(values)
+        if (mean.is_integer()):
+            return str(int(mean))
+        else:
+            return str(mean)
+    else:
+        return values
+
+
+@app.route('/avg')
+def avg():
+    values = Inputs_method()
+    if not type(values) is str:
+        mean = statistics.mean(values)
+        if (mean.is_integer()):
+            return str(int(mean))
+        else:
+            return str(mean)
+    else:
+        return values
+
+
+@app.route('/mean')
+def mean():
+    values = Inputs_method()
+    if not type(values) is str:
+        mean = statistics.mean(values)
+        if (mean.is_integer()):
+            return str(int(mean))
+        else:
+            return str(mean)
+    else:
+        return values
+
+        
 
 if __name__ == "__main__":
     app.run()
